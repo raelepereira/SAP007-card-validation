ef1a1d17deaaf58fea720ade3fd786c640018418
Raele Pereira <raelepereira.nutri@gmail.com>
2022-01-21 14:45:13 -0300

    adicionando icones

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import validator from "./validator.js";
-import ocultar from "./ocultar.js";
+import maskify from "./maskify.js";
 
 document.querySelector(".btn").addEventListener("click", Documentos);
 
 function Documentos() {
   let pegaValor = document.querySelector(".input").value;
-  document.querySelector(".resultado").innerHTML = ocultar.cardHide(pegaValor);
+  document.querySelector(".resultado").innerHTML = maskify.cardHide(pegaValor);
   
-  console.log(ocultar.cardHide(pegaValor))
+  console.log(maskify.cardHide(pegaValor))
   console.log(validator.isValid(pegaValor))
   
 }
+
+export default maskify;
+
