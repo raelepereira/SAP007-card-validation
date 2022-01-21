c63a26685b5d7d6c593da1e305584331eea859b8
Raele Pereira <raelepereira.nutri@gmail.com>
2022-01-21 14:43:36 -0300

    implementando a função maskify

---
+++ src/maskify.js
@@ -0,0 +1,16 @@
+//ocultar os números na tela e deixar apenas os últimos 4
+const maskify = {
+  cardHide(cardNumber) {
+    let hideNum = [];
+    for(let i = 0; i < cardNumber.length; i++){
+      if(i < cardNumber.length-4){
+        hideNum.push("#");
+      }else{
+        hideNum.push(cardNumber[i]);
+      }
+    }
+    return hideNum.join("");
+  }
+};
+
+export default maskify;
