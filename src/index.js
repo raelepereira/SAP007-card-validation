bf7dd4b17c7ec770bb48316e35c766066b7b21ee
Raele Pereira <raelepereira.nutri@gmail.com>
2022-02-02 18:27:14 -0300

    modificando o validator

--- src/index.js
+++ src/index.js
@@ -4,41 +4,47 @@ document.querySelector(".btn").addEventListener("click", Documentos);
 
 function Documentos() {
   let pegaValor = document.querySelector(".input").value;
-  document.querySelector(".resultado").innerHTML = maskify.cardHide(pegaValor);
+  document.querySelector(".resultado").innerHTML = validator.maskify(pegaValor);
   
-  console.log(maskify.cardHide(pegaValor))
+  console.log(validator.maskify(pegaValor))
   console.log(validator.isValid(pegaValor))
   
 }
 
 //função para ocultar os números e mostrar apenas os últimos 4.
-const maskify = {
-  cardHide(cardNumber) {
-    let hideNum = [];
-    for(let i = 0; i < cardNumber.length; i++){
-      if(i < cardNumber.length-4){
+
+/*const maskify = {
+  cardHide(cardNumber) {   //função cardHide // aceita CardNumber(string) como argumento
+    let hideNum = [];     //hideNum:  variável que a função irá retornar
+        for(let i = 0; i < cardNumber.length; i++){//após o término do loop, fica a matriz c os nu. do cartão par. ocultados 
+      if(i < cardNumber.length-4){ 
         hideNum.push("#");
       }else{
         hideNum.push(cardNumber[i]);
       }
     }
-    return hideNum.join("");
+    return hideNum.join(""); //método para converter o array em uma string
   }
 };
+*/
 
 //função para o input aceitar apenas números.
+
 var input = document.querySelector("#number");
-input.addEventListener("keypress", function(e) {
-    if(!checkChar(e)) {
-      e.preventDefault();
+input.addEventListener("keypress", function(e) { //saber qnd o usuário digitar algo
+    if(!checkChar(e)) { //checa o caracter, se vier falso, quer dizer caracter especial ...
+      e.preventDefault(); //limita o caracter especial de aparecer no input
   }
 });
-function checkChar(e) {
-    var char = String.fromCharCode(e.keyCode);
+
+function checkChar(e) { // (e)=evento
+    var char = String.fromCharCode(e.keyCode); //transformando em caracter
   
   console.log(char);
-    var pattern = '[0-9]';
-    if (char.match(pattern)) {
+    var pattern = '[0-9]'; //expressão regular, aceita apenas números
+    if (char.match(pattern)) { //se estiver dentro do padrão, retorna true
       return true;
   }
 }
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,21 +1,77 @@
-const validator = { 
-  isValid(CardNumber) { //método isValid recebe um cardNumber
+const validator  = {
+
+ //função para ocultar os números e mostrar apenas os últimos 4.
+    
+       maskify(cardNumber) {   //função cardHide // aceita CardNumber(string) como argumento
+      let hideNum = [];     //hideNum:  variável que a função irá retornar
+          for(let i = 0; i < cardNumber.length; i++){//após o término do loop, fica a matriz c os nu. do cartão par. ocultados 
+        if(i < cardNumber.length-4){ 
+          hideNum.push("#");
+        }else{
+          hideNum.push(cardNumber[i]);
+        }
+      }
+      return hideNum.join(""); //método para converter o array em uma string
+    },
+
+  
+    //algoritmo de luhn
+
+  isValid(CardNumber) { //método isValid 
     let arr = (CardNumber + '') // passa o CardNumber para uma variável
-      .split('') //.split separa cada elemento do array(números) para...(nesse caso, para um espaço vazio)
+      .split('') //.split separa cada elemento do array 
       .reverse() //.reverse troca todos os números de posição.
-      .map(x => parseInt(x)); //.map pode fazer uma função dentro do seu array/lista para cada objeto.
-    let lastDigit = arr.splice(0, 1)[0]; //.splice pode adicionar e remover itens de uma lista.
-    let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0);
-    sum += lastDigit; // Passando a soma para a variável; .reduce soma todos os itens da lista.
+      .map(x => parseInt(x)); //converte uma string em um inteiro
+    let lastDigit = arr.splice(0, 1)[0]; //passa o nu. pra uma variável .splice pode adicionar e remover itens de uma lista.
+    let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0); // .reduce soma todos os itens da lista.
+    sum += lastDigit; 
     if(sum % 10 === 0){ //Se for um múltiplo de 10 o cartão é válido, se não, é inválido.
       window.alert("Cartão Válido")
-    } else {
+      return true
+     
+    } else { 
       window.alert("Cartão Inválido")
+      return false
+      
     }
   },
 };
 
+
 export default validator;
 
 
-//parseInt (está parseando cada objeto dessa lista para um Int (número), ou seja, está passando as 
+
+
+/*
+function checkLuhn(cardNo)
+    {
+        let nDigits = cardNo.length;
+ 
+        let nSum = 0;
+        let isSecond = false;
+        for (let i = nDigits - 1; i >= 0; i--)
+        {
+ 
+            let d = cardNo[i].charCodeAt() - '0'.charCodeAt();
+ 
+            if (isSecond == true)
+                d = d * 2;
+ 
+            // We add two digits to handle
+            // cases that make two digits
+            // after doubling
+            nSum += parseInt(d / 10, 10);
+            nSum += d % 10;
+ 
+            isSecond = !isSecond;
+        }
+        return (nSum % 10 == 0);
+    }
+     
+    let cardNo = "79927398713";
+    if (checkLuhn(cardNo))
+      document.write("This is a valid card");
+    else
+      document.write("This is not a valid card");
+      */
